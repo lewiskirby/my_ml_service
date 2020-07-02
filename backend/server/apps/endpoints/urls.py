6f287cf42f33f2eb8d1f94f0f4585d59d431c097
Lewis <lewiskirby@hotmail.co.uk>
2020-07-02 14:08:20 +0100

    endpoints models

---
+++ backend/server/apps/endpoints/urls.py
@@ -0,0 +1,26 @@
+"""
+The code below will create REST API routers to our database models.
+
+Our models will be accessed by following the URL pattern:
+http://<server-ip>/api/v1/<object-name>
+
+You can notice that we include v1 in the API address. This might be needed later for API versioning.
+"""
+
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
+from apps.endpoints.views import EndpointViewSet
+from apps.endpoints.views import MLAlgorithmViewSet
+from apps.endpoints.views import MLAlgorithmStatusViewSet
+from apps.endpoints.views import MLRequestViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+urlpatterns = [
+    url(r"^api/v1/", include(router.urls))
+]
